3b946cac7c2770825d5ed1857ad4bc3c7f71a23a
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-09-23 18:01:26 +0500

    FEAT: Add Docker Compose

--- Api/Program.cs
+++ Api/Program.cs
@@ -2,15 +2,23 @@ using Api.Configuration;
 using Api.Middlewares;
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Controllerlarni ishlashi uchun kerak
+// Controllerlarni ulash
 builder.Services.AddControllers();
 
-// Api dokumentatsiyasi uchun kerak
+// Swagger (OpenAPI) ulash
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "TestProject API",
+        Version = "v1"
+    });
+});
 
 // Loyihaga oid servislarni ulash
 builder.Services.AddDbContext(builder.Configuration);
@@ -18,31 +26,35 @@ builder.Services.AddProjectServices(builder.Configuration);
 
 var app = builder.Build();
 
-// Ma'lumotlar bazasini migratsiya qilish
+// Database migratsiya qilish
 using (var scope = app.Services.CreateScope())
 {
     var dataContext = scope.ServiceProvider.GetRequiredService<DataContext>();
     await dataContext.Database.MigrateAsync();
 }
 
-// Exception handler middleware
+// Global exception handler middleware
 app.UseMiddleware<ExceptionHandlerMiddleware>();
 
-// Swagger faqat Developmentda ochiladi
-if (app.Environment.IsDevelopment())
+// Swagger Development va Production muhitida ochiladi
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "TestProject API v1");
+        c.RoutePrefix = string.Empty; // Swagger ochilganda bevosita root URL’dan ochiladi
+    });
 }
 
 app.UseStaticFiles();
 app.UseDefaultFiles();
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection(); // Docker ichida kerak bo‘lmasa qoldirib ketishingiz mumkin
 
 app.UseAuthentication();
 app.UseAuthorization();
 
 // Controllerlarni ulash
-app.MapControllers();   // <<<--- MUHIM
+app.MapControllers();
 
 app.Run();
